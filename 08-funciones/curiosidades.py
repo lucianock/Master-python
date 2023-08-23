22ae18667025978f215613174a917bd8d3be968b
Luciano Campos Kriegl <62312023+lucianocamposk@users.noreply.github.com>
2023-08-23 14:47:33 -0300

    sql migration

--- 08-funciones/curiosidades.py
+++ 08-funciones/curiosidades.py
@@ -1,11 +1,20 @@
 def mi_funcion(nombre):
+    print ('hola que tal')
     return "Hola que tal " +nombre
 
 def mi_segunda_funcion(apellidos):
     return "Hola que tal 2 " + apellidos
 
-nombre= "luciano"
-apellidos="campos kriegl"
+nombre = "luciano"
+apellidos = "campos kriegl"
 
 print(mi_funcion(nombre))
+print(mi_funcion(nombre))
+print(mi_funcion(nombre))
+print(mi_funcion(nombre))
+print(mi_funcion(nombre))
+print(mi_funcion(nombre))
+
+
+
 print(mi_segunda_funcion(apellidos))
